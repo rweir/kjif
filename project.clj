fa8feb47636ac73fabc31fff3d54944f10d9cacd
Rob Weir <rweir@ertius.org>
2014-02-15 17:32:08 +1100

    more plugins

--- project.clj
+++ project.clj
@@ -7,7 +7,12 @@
                  [http-kit "2.1.16"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [org.clojure/core.typed "0.2.31"]]
-  :plugins [[lein-typed "0.3.1"]]
+  :plugins [[lein-typed "0.3.1"]
+            [lein-checkall "0.1.1"]
+            [lein-difftest "2.0.0"]
+            [lein-test-out "0.3.0"]
+            [lein-licenses "0.1.1"]
+            [theladders/lein-uberjar-deploy "0.1.4"]]
   :core.typed {:check [kjif.core]}
   :main ^:skip-aot kjif.core
   :target-path "target/%s"
