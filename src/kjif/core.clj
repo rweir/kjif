39d9e9893fbd711eab19fc26a021657e325f5d4e
Rob Weir <rweir@ertius.org>
2014-02-15 17:32:18 +1100

    lots of typing

--- src/kjif/core.clj
+++ src/kjif/core.clj
@@ -1,25 +1,29 @@
 (ns kjif.core
   (:require
-   [clojure.core.typed :refer [ann check-ns]])
+   [clojure.core.typed :refer [ann check-ns def-alias Map]])
   (:gen-class))
 
-(ann set-dark-feature [Hostname -> (IPersistentMap String String)])
+(def-alias Hostname String)
+(def-alias InfoMap '[Map clojure.lang.Keyword Any])
+(def-alias PassMap [InfoMap -> InfoMap])
+
+(ann set-dark-feature PassMap)
 (defn set-dark-feature
   "Set a dark feature to a particular value"
   [data]
   (println (format "Setting a dark feature value" (:hostname data) data))
-  (assoc data :seen (conj (:seen data) "set-dark-feature")))
+  data)
 
-(ann get-dark-feature [Hostname -> (IPersistentMap String String)])
+(ann get-dark-feature PassMap)
 (defn get-dark-feature
   "Get the value of a dark feature"
   [data]
   (println (format "Getting a dark feature value" (:hostname data) data))
-  (assoc data :seen (conj (:seen data) "get-dark-feature")))
+  data)
 
-(ann -main [-> nil] )
+(ann ^:no-check -main [String * -> nil])
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Run the provided set of steps"
   [& args]
   (let [hosts [{:hostname "foo.atlassian.net"} {:hostname "bar.atlassian.net"}]]
     (doall (map (fn [host]
