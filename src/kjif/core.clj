172a9be654baf565b1d2e1e3dfceff8462c483a3
Rob Weir <rweir@ertius.org>
2014-02-15 22:33:26 +1100

    basic folding

--- src/kjif/core.clj
+++ src/kjif/core.clj
@@ -14,27 +14,44 @@
   [data]
   (println (format "Setting a dark feature value" (:hostname data) data))
   (assoc-in data [:set-dark-feature "feature"] "XXX")
-  data)
+  (assoc data :set-d true))
 
 (ann get-dark-feature PassMap)
 (defn get-dark-feature
   "Get the value of a dark feature"
   [data]
   (println (format "Getting a dark feature value" (:hostname data) data))
-  data)
+  (assoc data :get-d true))
+
+(defn exec-step
+  [data step]
+  (let [result (step data)]
+    (println (format "Exec'ing step: %s with data: %s and result: %s" step data result))
+    (assoc data :saw (conj (:saw data) step))
+    result))
+
+(defn do-one-host
+  "Run the steps over one host"
+  [host steps]
+  (reduce exec-step {:hostname host} steps))
+
+(defn do-all-hosts
+  "Run the steps over the hosts"
+  [hosts steps]
+  (let [result {}]
+    (merge (map #(do-one-host % steps) hosts))))
+
+(def-alias doit do-all-hosts)
 
 (ann ^:no-check -main [String * -> nil])
 (defn -main
   "Run the provided set of steps"
   [& args]
   (let [hosts [{:hostname "foo.atlassian.net"} {:hostname "bar.atlassian.net"}]]
-    (let [results (map (fn [host]
-                         (-> host
-                             (set-dark-feature)
-                             (get-dark-feature)))
-                       hosts)]
-          (pprint results))))
+    (let [steps [set-dark-feature get-dark-feature]
+          results (doit hosts steps)]
+      (pprint results))))
 
 (comment
   (check-ns)
-)
+  )
--- test/kjif/core_test.clj
+++ test/kjif/core_test.clj
@@ -5,3 +5,22 @@
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 1 1))))
+
+(deftest test-do-one-host
+  (testing "Empty steps"
+    (is (= (do-one-host [] [])
+           {}))))
+
+(deftest executor
+  (testing "Empty step list"
+    (let [hosts ["foo.x.com" "bar.x.com"]
+          steps []]
+      (is (= (do-all-hosts hosts steps)
+          {"foo.x.com" {}
+           "bar.x.com" {}
+           }))))
+  (testing "Empty host list"
+    (let [hosts []
+          steps [set-dark-feature]]
+      (is (= (do-all-hosts hosts steps)
+          {})))))
