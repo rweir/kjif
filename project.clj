6623299b8dc033f5c86112d56f4f8e6cae0d9c1e
Rob Weir <rweir@ertius.org>
2014-02-15 15:38:58 +1100

    add deps

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [http-kit "2.1.16"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [org.clojure/core.typed "0.2.30"]]
+  :plugins [[lein-typed "0.3.1"]]
   :main ^:skip-aot kjif.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
