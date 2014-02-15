ec5e0684d578acacdc373e1ef9f816548f4fe388
Rob Weir <rweir@ertius.org>
2014-02-15 15:55:29 +1100

    annotate

--- src/kjif/core.clj
+++ src/kjif/core.clj
@@ -1,7 +1,14 @@
 (ns kjif.core
+  (:require
+   [clojure.core.typed :refer [ann check-ns]])
   (:gen-class))
 
+(ann -main [-> nil] )
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(comment
+  (check-ns)
+)
